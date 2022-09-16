98e982a63260613ff8c5776fc7e26e5e2259a921
PurinCompattana <jkshot000@gmail.com>
2022-09-16 14:44:28 +0700

    get fiber version 2 packages

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main(){
-	fmt.Print("hello")
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
+
+	app.Post("search", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"message":"Hello"})
+	})
+
+	app.Listen(":3001")
 }
