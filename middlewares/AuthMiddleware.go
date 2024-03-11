5d4692d300fb521bd1ad3562f86241cd0f780535
PurinCompattana <dev.purin@gmail.com>
2024-03-11 23:06:18 +0700

    feat: create AuthMiddleware

---
+++ middlewares/AuthMiddleware.go
@@ -0,0 +1,36 @@
+package middlewares
+
+import (
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+)
+
+const SecretKey = "123456789"
+
+func AuthMiddleware(c *fiber.Ctx) error {
+	authorizationHeader := c.Get("Authorization")
+
+	parts := strings.Split(authorizationHeader, " ")
+	if len(parts) != 2 {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"message": "Invalid Authorization header format"})
+	}
+
+	token := parts[1]
+
+	if token == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Token is not found", "token": token})
+	}
+
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Token validation failed", "error": err.Error()})
+	}
+
+	return c.Next()
+}
