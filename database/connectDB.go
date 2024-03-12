f3df68c5094621ffa9a8351a39c256c83d51e818
PurinCompattana <dev.purin@gmail.com>
2024-03-12 22:57:15 +0700

    chore: get env data config database

--- database/connectDB.go
+++ database/connectDB.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/PurinPintakhiew/Golang-API/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -10,12 +12,20 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "root:@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
+	host := os.Getenv("DB_HOST")
+	username := os.Getenv("DB_USERNAME")
+	password := os.Getenv("DB_PASSWORD")
+	databaseName := os.Getenv("DB_DATABASE")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		username, password, host, port, databaseName)
+
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		fmt.Println("failed to connect database")
+		fmt.Println("failed to connect database:", err)
 	} else {
-		fmt.Println("connected database")
+		fmt.Println("connected to database")
 		DB = db
 		db.AutoMigrate(&models.Users{})
 	}
