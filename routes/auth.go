d5c5f9b4b0c494b487ac986971874f4c85598b2f
PurinPintakhiew <dev.purin@gmail.com>
2024-03-13 23:29:25 +0700

    chore: edit auth route

--- routes/auth.go
+++ routes/auth.go
@@ -2,31 +2,24 @@ package routes
 
 import (
 	"github.com/PurinPintakhiew/Golang-API/controllers"
-	"github.com/PurinPintakhiew/Golang-API/database"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Route() {
-	database.ConnectDB()
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://127.0.0.1:3000, http://localhost:3000",
 		AllowCredentials: true,
 	}))
 
-	// public display
-	app.Static("/static", "./public")
-
-	// route path Hello
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{"message": "Hello, Purin"})
+		return c.JSON(fiber.Map{"message": "This request does not exist."})
 	})
 
-	// Authentication api ประกาศ
-	app.Post("/register", controllers.Register) // ลงทะเบียนหรือเพิ่มผู้ใช้
-	app.Post("/login", controllers.Login)       // ล็อคอิน
-	app.Post("/logout", controllers.Logout)     // ออกจากระบบ
+	app.Post("/api/register", controllers.Register)
+	app.Post("/api/login", controllers.Login)
 
 	app.Listen((":8080"))
 }
