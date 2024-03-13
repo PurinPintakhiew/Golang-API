dc1fc96f7a8ee92b65fd3d845f32482ec772dd93
PurinPintakhiew <dev.purin@gmail.com>
2024-03-13 23:27:43 +0700

    chore: add Connect DB

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/PurinPintakhiew/Golang-API/configs"
 	"github.com/PurinPintakhiew/Golang-API/routes"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
+	configs.ConnectDB()
 	routes.Route()
 }
